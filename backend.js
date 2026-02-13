c2a2ef2451851a816394c0d0343c0bcd0870979a
AndresD902 <posadaandres543@gmail.com>
2026-02-13 14:45:29 -0500

    Add check balance logic

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
 const checkButton = document.querySelector(".check");
 
-// CHECK BALANCE
+
 checkButton.addEventListener("click", () => {
     balanceText.textContent = `$${balance.toFixed(2)}`;
     messageText.textContent = "Current balance displayed successfully.";
